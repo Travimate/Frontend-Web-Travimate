b65e094f15143509ef1e323479c9a0368365e9db
rareaqil <rareaqil123@gmail.com>
2024-02-08 19:31:43 +0700

    update card component

--- src/components/layout-clients/part/inputfield.tsx
+++ src/components/layout-clients/part/inputfield.tsx
@@ -1,9 +1,12 @@
+import FlightCard from "./FlightCard"
 import DateSlider from "./dateSlider"
 
 const InputField = () => {
   return (
-    <div id="section-a"className="items-center">
+    <div id="section-a"className="items-center bg-blue-200">
     <DateSlider/>
+    <FlightCard/>
+    <FlightCard/>
     </div>
   )
 }
